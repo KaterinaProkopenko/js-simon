da839ae60e877d6cbc62217dc9bbad4ed1f421ec
KaterinaProkopenko <ekaprokopenko97@gmail.com>
2022-03-14 15:51:56 +0100

    Aggiornamento

--- js/script.js
+++ js/script.js
@@ -1,15 +1,12 @@
-const functionNumbers = generateNumber(5);
+const numbers = [];
 
-function generateNumber(number){
-    const numbers = [];
-    for(let i = 0; i < number; i++){
-        numbers.push(generateRandomNumber(numbers, 1, 50));
-    }
-    document.getElementById('user-number').innerHTML = `${numbers[0]} - ${numbers[1]} - ${numbers[2]} - ${numbers[3]} - ${numbers[4]}`;
-    console.log(numbers)
-}
+for(let i = 0; i < 5; i++){
+    numbers.push(generateRandomNumber(numbers, 1, 20));
+};
 
-/* Non funziona il flag */
+document.getElementById('user-number').innerHTML = `${numbers[0]} - ${numbers[1]} - ${numbers[2]} - ${numbers[3]} - ${numbers[4]}`;
+console.log(numbers);
+
 function generateRandomNumber(blackList, min, max){
     let check = false;
     let randomInt;
@@ -19,20 +16,44 @@ function generateRandomNumber(blackList, min, max){
 
         if(!blackList.includes(randomInt)){
             check = true;
-        }
+        };
         return randomInt;
-    }
-}
+    };
+};
 
 setTimeout(hidden, 2999);
 
 function hidden() {
     document.getElementById('user-number').innerHTML = '';
-}
+};
 
-setTimeout(userNumbers, 3000);
+setTimeout(userNumbersFunction, 3000, numbers);
 
-function userNumbers() {
-    parseInt(prompt('Inserisci i numeri che hai visto'));
-}
+function userNumbersFunction(givenNumbers) {
+    const userNumbers = [];
+    const guessedNumbers = [];
 
+    for (let i = 0; i < 5; i++){
+        const userNumber = parseInt(prompt('Inserisci i numeri che hai visto'));
+        if (!isNaN(userNumber)){
+            userNumbers.push(userNumber);
+            if(givenNumbers.includes(userNumber)){
+                pushUnique(guessedNumbers, userNumber);
+            };
+        };
+    };
+
+    console.log(userNumbers);
+    console.log(guessedNumbers);
+
+    document.getElementById('user-number').innerHTML = 
+    `Numeri che hai indovinato: ${guessedNumbers.join(', ')} <br>
+     Numeri mostrati: ${givenNumbers.join(', ')}`;
+};
+
+function pushUnique(array, elementToAdd){
+    if (!array.includes(elementToAdd)){
+        array.push(elementToAdd);
+        return array;
+    };
+};
